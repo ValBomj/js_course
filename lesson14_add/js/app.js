7100f23ec23dbe4f248de48f961fb5ef7ab57ae4
ValBomj <123itunes2002@gmail.com>
2020-06-26 00:52:31 +0300

    fix class comstructor

--- lesson14_add/js/app.js
+++ lesson14_add/js/app.js
@@ -1,36 +1,25 @@
 'use strict';
 const body = document.querySelector('body');
 
-const DomElement = function(s, h, w, b, f) {
+function DomElement(s = '.block', h = 100, w = 100, b = 'blue', f = 24) {
   this.selector = s;
   this.height = h;
   this.width = w;
   this.bg = b;
   this.fontSize = f;
-  this.createElement = function() {
-    if (this.selector[0] === '.') {
-      const div = document.createElement('div');
-      div.className = this.selector.slice(1, this.selector.length);
-      div.style.cssText = 'height: ' + this.height + 'px' + ';' + 
-                    'width: ' + this.width + 'px' + ';' +
-                    'background: ' + this.bg + ';' +
-                    'font-size: ' + this.fontSize + 'px' + ';';
-      div.textContent = this.selector.slice(1, this.selector.length);
-      body.appendChild(div);
-    } else if (this.selector[0] === '#') {
-      const p = document.createElement('p');
-      p.id = this.selector.slice(1, this.selector.length);
-      p.style.cssText = 'height: ' + this.height + 'px' + ';' + 
-                  'width: ' + this.width + 'px' + ';' +
-                  'background: ' + this.bg + ';' +
-                  'font-size: ' + this.fontSize + 'px' + ';';
-      p.textContent = this.selector.slice(1, this.selector.length);
-      body.appendChild(p);
-    }
-  }
-  if (this.selector) {
-    this.createElement();
-  }
 }
+// .className = this.selector.slice(1, this.selector.length)
+DomElement.prototype.createElement = function() {
+  const block = this.selector[0] === '.' ? document.createElement('div') : this.selector[0] === '#' ? document.createElement('p') : console.log('Элемент не создан');
+  const blockSelector = this.selector.slice(1, this.selector.length);
+  this.selector[0] === '.' ? block.className = blockSelector : this.selector[0] === '#' ? block.id = blockSelector : console.log('Элемент не создан');
+  block.style.cssText = 'height: ' + this.height + 'px' + ';' + 
+                        'width: ' + this.width + 'px' + ';' +
+                        'background: ' + this.bg + ';' +
+                        'font-size: ' + this.fontSize + 'px' + ';';
+  block.textContent = blockSelector;
+  body.appendChild(block);
+};
 
-const domElement = new DomElement('.block', 100, 400, 'blue', 32);
+const domElement = new DomElement();
+domElement.createElement();
