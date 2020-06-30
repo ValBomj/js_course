8a7f1fefb10a3f6687d668a516db8fdee2cd5f8c
ValBomj <123itunes2002@gmail.com>
2020-06-30 19:22:59 +0300

    task 3

--- lesson17/js/app.js
+++ lesson17/js/app.js
@@ -1,14 +1,38 @@
 'use strict';
-function getResult(x,y){
-  let result;
-  
-  const pow = (Math.pow(x,y) + '').split('');
 
-  const powToNum = pow.map((item) => +item)
-  
-  result = powToNum.reduce((acc, item) => acc + item);
+const countrySelect = document.querySelector('#country');
+const citySelect = document.querySelector('#city');
+const result = document.querySelector('.result');
 
-  return result
+const cityArr = {
+  rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
+  uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
+  bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест', 'ф', 'фdasd', 'qweqe'],
+  jap: ['Токио', 'Киото', 'Осака', 'Иокогама'] 
 }
 
-console.log(getResult(7, 10))
+function init() {
+  const currentCountry = countrySelect[countrySelect.selectedIndex].textContent;
+  const currentCity = citySelect[citySelect.selectedIndex].textContent;
+  result.textContent = `${currentCountry} ${currentCity}`;
+}
+
+function addCities() {
+  const currentCountryCities = cityArr[countrySelect.value];
+  citySelect.innerHTML = '';
+
+  currentCountryCities.forEach((item, i, arr) => {
+    const option = document.createElement('option');
+    option.value = countrySelect.value;
+    option.textContent = cityArr[countrySelect.value][i];
+    citySelect.appendChild(option);
+  })
+
+  init();
+  citySelect.style.display = 'inline-block';
+}
+
+countrySelect.addEventListener('input', addCities);
+
+citySelect.addEventListener('input', init)
+
